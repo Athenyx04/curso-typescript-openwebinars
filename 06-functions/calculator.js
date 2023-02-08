a31c45ec455ab914878feee1c3464e17c5a9cf61
Manuel Calvo <github.t@owldesk.top>
2023-02-08 12:44:40 +0100

    Complete 06-functions section

--- 06-functions/calculator.js
+++ 06-functions/calculator.js
@@ -1,113 +1,91 @@
-const add = (a, b) => {
-  return a + b;
+var add = function (a, b) {
+    return a + b;
 };
-
-const subtract = (a, b) => {
-  return a - b;
+var subtract = function (a, b) {
+    return a - b;
 };
-
-const divide = (a, b) => {
-  if (b === 0) {
-    throw new Error('Division by zero is undefined');
-  }
-
-  return a / b;
+var divide = function (a, b) {
+    if (b === 0) {
+        throw new Error('Division by zero is undefined');
+    }
+    return a / b;
 };
-
-const multiply = (a, b) => {
-  return a * b;
+var multiply = function (a, b) {
+    return a * b;
 };
-
 /**
  * Should define Actions[] types
  */
-const ACTIONS = [
-  {
-    name: 'ADD',
-    do: add,
-  },
-  {
-    name: 'SUBTRACT',
-    do: subtract,
-  },
-  {
-    name: 'DIVIDE',
-    do: divide,
-  },
-  {
-    name: 'MULTIPLY',
-    do: multiply,
-  },
+var ACTIONS = [
+    {
+        name: 'ADD',
+        "do": add
+    },
+    {
+        name: 'SUBTRACT',
+        "do": subtract
+    },
+    {
+        name: 'DIVIDE',
+        "do": divide
+    },
+    {
+        name: 'MULTIPLY',
+        "do": multiply
+    },
 ];
-
-// Element is type HTMLElement
 function updateUserMoneyText(element, actualMoney) {
-  element.innerHTML = actualMoney;
+    element.innerHTML = actualMoney.toString();
 }
-
 function randomNumber(max) {
-  return Math.round(Math.random() * max);
+    return Math.round(Math.random() * max);
 }
-
-function playTheGame({ actions, totalClick, onError, onSuccess, userMoney }) {
-  const randomIndex = randomNumber(actions.length);
-  const A_MILLION = 1000000;
-  const action = actions[randomIndex];
-
-  if (!action) {
-    onError(randomIndex, actions);
-    return userMoney;
-  }
-
-  if (userMoney >= A_MILLION) {
-    if (typeof onSuccess === 'function') {
-      onSuccess(totalClick);
+function playTheGame(_a) {
+    var actions = _a.actions, totalClick = _a.totalClick, onError = _a.onError, onSuccess = _a.onSuccess, userMoney = _a.userMoney;
+    var randomIndex = randomNumber(actions.length);
+    var A_MILLION = 1000000;
+    var action = actions[randomIndex];
+    if (!action) {
+        onError(randomIndex, actions);
+        return userMoney;
     }
-
-    return userMoney;
-  }
-
-  if (userMoney <= 0) {
-    throw new Error('Money must be positive');
-  }
-
-  const newMoney = Math.round(action.do(userMoney, randomNumber(actions.length * 100)));
-
-  return newMoney;
+    if (userMoney >= A_MILLION) {
+        if (typeof onSuccess === 'function') {
+            onSuccess(totalClick);
+        }
+        return userMoney;
+    }
+    if (userMoney <= 0) {
+        throw new Error('Money must be positive');
+    }
+    var newMoney = Math.round(action["do"](userMoney, randomNumber(actions.length * 100)));
+    return newMoney;
 }
-
 function disableClickButton($button, handleClick) {
-  $button.disabled = true;
-
-  if (typeof handleClick === 'function') {
-    $button.removeEventListener('click', handleClick);
-  }
+    $button.disabled = true;
+    if (typeof handleClick === 'function') {
+        $button.removeEventListener('click', handleClick);
+    }
 }
-
-const $button = document.getElementById('button');
-const $userMoneyText = document.getElementById('moneyText');
-let userMoney = 1000;
-let totalClick = 0;
-
+var $button = document.getElementById('button');
+var $userMoneyText = document.getElementById('moneyText');
+var userMoney = 1000;
+var totalClick = 0;
 updateUserMoneyText($userMoneyText, userMoney);
-
 $button.addEventListener('click', function handleClick() {
-  totalClick++;
-
-  const params = {
-    actions: ACTIONS,
-    totalClick,
-    userMoney: userMoney,
-    onError: function (index, actions) {
-      console.log(index, actions);
-    },
-    onSuccess: function (totalClick) {
-      disableClickButton($button, handleClick);
-      console.log('Te has convertido en millonario al hacer un total de: ', totalClick, ' clicks');
-    },
-  };
-
-  userMoney = playTheGame(params);
-
-  updateUserMoneyText($userMoneyText, userMoney);
+    totalClick++;
+    var params = {
+        actions: ACTIONS,
+        totalClick: totalClick,
+        userMoney: userMoney,
+        onError: function (index, actions) {
+            console.log(index, actions);
+        },
+        onSuccess: function (totalClick) {
+            disableClickButton($button, handleClick);
+            console.log('Te has convertido en millonario al hacer un total de: ', totalClick, ' clicks');
+        }
+    };
+    userMoney = playTheGame(params);
+    updateUserMoneyText($userMoneyText, userMoney);
 });
---
+++ 06-functions/calculator.ts
@@ -0,0 +1,143 @@
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+const divide = (a: number, b: number): number | never => {
+  if (b === 0) {
+    throw new Error('Division by zero is undefined');
+  }
+
+  return a / b;
+};
+
+const multiply = (a: number, b: number): number => {
+  return a * b;
+};
+
+interface Action {
+  name: string;
+  do: (a: number, b: number) => number | never;
+}
+
+/**
+ * Should define Actions[] types
+ */
+const ACTIONS: Action[] = [
+  {
+    name: 'ADD',
+    do: add,
+  },
+  {
+    name: 'SUBTRACT',
+    do: subtract,
+  },
+  {
+    name: 'DIVIDE',
+    do: divide,
+  },
+  {
+    name: 'MULTIPLY',
+    do: multiply,
+  },
+];
+
+function updateUserMoneyText(
+  element: HTMLInputElement,
+  actualMoney: number
+): void {
+  element.innerHTML = actualMoney.toString();
+}
+
+function randomNumber(max: number): number {
+  return Math.round(Math.random() * max);
+}
+
+interface PlayTheGameParams {
+  actions: Action[];
+  totalClick: number;
+  userMoney: number;
+  onError: (randomIndex: number, actions: Action[]) => void;
+  onSuccess?: (totalClick: number) => void;
+}
+
+function playTheGame({
+  actions,
+  totalClick,
+  onError,
+  onSuccess,
+  userMoney,
+}: PlayTheGameParams): number | never {
+  const randomIndex = randomNumber(actions.length);
+  const A_MILLION = 1000000;
+  const action = actions[randomIndex];
+
+  if (!action) {
+    onError(randomIndex, actions);
+    return userMoney;
+  }
+
+  if (userMoney >= A_MILLION) {
+    if (typeof onSuccess === 'function') {
+      onSuccess(totalClick);
+    }
+
+    return userMoney;
+  }
+
+  if (userMoney <= 0) {
+    throw new Error('Money must be positive');
+  }
+
+  const newMoney = Math.round(
+    action.do(userMoney, randomNumber(actions.length * 100))
+  );
+
+  return newMoney;
+}
+
+function disableClickButton(
+  $button: HTMLButtonElement,
+  handleClick?: () => void
+) {
+  $button.disabled = true;
+
+  if (typeof handleClick === 'function') {
+    $button.removeEventListener('click', handleClick);
+  }
+}
+
+const $button = document.getElementById('button') as HTMLButtonElement;
+const $userMoneyText = document.getElementById('moneyText') as HTMLInputElement;
+let userMoney = 1000;
+let totalClick = 0;
+
+updateUserMoneyText($userMoneyText, userMoney);
+
+$button.addEventListener('click', function handleClick() {
+  totalClick++;
+
+  const params: PlayTheGameParams = {
+    actions: ACTIONS,
+    totalClick,
+    userMoney: userMoney,
+    onError: function (index, actions) {
+      console.log(index, actions);
+    },
+    onSuccess: function (totalClick) {
+      disableClickButton($button, handleClick);
+      console.log(
+        'Te has convertido en millonario al hacer un total de: ',
+        totalClick,
+        ' clicks'
+      );
+    },
+  };
+
+  userMoney = playTheGame(params);
+
+  updateUserMoneyText($userMoneyText, userMoney);
+});
